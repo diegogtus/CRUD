0296098ee18a466e05ae7acf415ad235f6513937
diegogtus <alejandro.diego.gt@gmail.com>
2019-11-13 14:36:02 -0600

    Update note_routes.js
    Collection modified

--- Node/app/routes/note_routes.js
+++ Node/app/routes/note_routes.js
@@ -4,13 +4,11 @@ app.get('/api/v1/cars/', (req, res) => {
     const id = req.params.id;
     var ObjectId = require('mongodb').ObjectID;
     const details = { '_id': new ObjectId(id) };
-    //db.collection('cars').find({}).toArray(function(err, result) {
-    db.collection("cars").find({}).toArray(function(err, result) {
+    db.collection("transport").find({}).toArray(function(err, result) {
     if (err) {        
        res.send({'error':'An error has occurred'});      } 
     else {
-       res.status(200).json(result);     
-       //console.log(result); 
+       res.status(200).json(result);      
        }     
    });  
 });
@@ -20,7 +18,7 @@ app.get('/api/v1/cars/', (req, res) => {
            const id = req.params.id; 
            var ObjectId = require('mongodb').ObjectID;  
            const details = {'_id': new ObjectId(id) };
-                    db.collection('cars').remove(details, (err, item) => {
+                    db.collection('transport').remove(details, (err, item) => {
                             if (err) {  
                                 res.send({ 'error': 'An error has occurred' });      
                                 res.status(404);
@@ -36,11 +34,12 @@ app.put('/api/v1/cars/:id', (req, res) => {
     const id = req.params.id;
     var ObjectId = require('mongodb').ObjectID;
     const details = { '_id': new ObjectId(id) };
-    const cars = { brand: req.body.brand, year: req.body.year, displacement: req.body.displacement, description: req.body.description  };
-    db.collection('cars').update(details, cars, (err, result) =>{
+    const cars = { brand: req.body.brand, model: req.body.model, year: req.body.year, displacement: req.body.displacement, description: req.body.description,
+        path: req.body.path  };
+    db.collection('transport').update(details, cars, (err, result) =>{
              if (err) {          
                 res.status(404);      }
-              else {         // res.send(cars);
+              else {         
                  res.status(201).json({status:"Actualizado"});
                  }     
              });  
@@ -51,7 +50,7 @@ app.put('/api/v1/cars/:id', (req, res) => {
              const id = req.params.id;
              var ObjectId = require('mongodb').ObjectID;
              const details = { '_id': new ObjectId(id) };
-             db.collection('cars').findOne(details, (err, item) => {
+             db.collection('transport').findOne(details, (err, item) => {
              if (err) {        
                 res.send({'error':'An error has occurred'});      } 
              else {
@@ -62,8 +61,9 @@ app.put('/api/v1/cars/:id', (req, res) => {
         });
 //CREATE
      const collection =   app.post('/api/v1/cars/', (req, res) => {    
-         const cars = { brand: req.body.brand, year: req.body.year, displacement: req.body.displacement, description: req.body.description };    
-         db.collection('cars').insert(cars, (err, result) => { 
+         const cars = { brand: req.body.brand, model: req.body.model, year: req.body.year, displacement: req.body.displacement, description: req.body.description,
+             path: req.body.path };    
+         db.collection('transport').insert(cars, (err, result) => { 
                   if (err) {         res.send({ 'error': 'An error has occurred' });       } 
                   else {
                             res.status(201).json(cars);
